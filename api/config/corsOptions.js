a8ca8e270c5f4be57e52f67c8ec9c99a09eca8fe
developer-shajib <sunhailshajib1@gmail.com>
2024-02-13 22:51:19 +0600

    updated client and server side link

--- api/config/corsOptions.js
+++ api/config/corsOptions.js
@@ -1,12 +1,6 @@
 // Cross Configure
 export const corsOptions = {
-  origin: [
-    'http://localhost:5050',
-    'http://localhost:5173',
-    'http://localhost:3000',
-    'https://tours-and-travel-booking.vercel.app',
-    'https://tours-and-travels-booking.netlify.app',
-  ],
+  origin: ['http://localhost:5050', 'http://localhost:5173', 'http://localhost:3000', 'https://tour-travel-booking.vercel.app'],
   credentials: true,
-  optionsSuccessStatus: 200,
+  optionsSuccessStatus: 200
 };
--- client/src/utils/appUrl.js
+++ client/src/utils/appUrl.js
@@ -1,2 +1,2 @@
-export const BASE_URI = 'https://tours-and-travels-booking.onrender.com/api/v1';
+export const BASE_URI = 'https://tour-and-travel-booking.onrender.com/api/v1';
 // export const BASE_URI = 'http://localhost:5050/api/v1';
